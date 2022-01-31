8445175aa1ebb605d788cc90f379c2c689185566
Jesse Lawler <jedd.lawler@gmail.com>
2022-01-31 12:26:12 -0700

    Built models, building routes

--- models/index.js
+++ models/index.js
@@ -4,6 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
+ 
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
+
 // Products belongsTo Category
 
 // Categories have many Products
